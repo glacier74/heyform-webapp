b2138078f3dc255bd1e4993db30ededfd45682ec
Glacier <11812405+glacier74@users.noreply.github.com>
2022-02-15 21:58:47 +0800

    fix(ui): fix Menu typing error

--- ui/src/menu/index.ts
+++ ui/src/menu/index.ts
@@ -2,14 +2,15 @@ import type { FC } from 'react'
 import MenuDivider from './Divider'
 import type { MenuItemProps } from './Item'
 import MenuItem from './Item'
+import type { MenusProps } from './Menus'
 import Menus from './Menus'
 
-type ExportMenusType = FC<IComponentProps> & {
+type ExportMenusType = FC<MenusProps> & {
   Item: FC<MenuItemProps>
-  Divider: FC<IComponentProps>
+  Divider: FC<IComponentProps<HTMLDivElement>>
 }
 
-const ExportMenus = Menus as ExportMenusType
+const ExportMenus = Menus as unknown as ExportMenusType
 ExportMenus.Item = MenuItem
 ExportMenus.Divider = MenuDivider
 
