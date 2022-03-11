4d1f1ec831d0b93c62aa2718fac4a3cdffcf2bf3
Glacier <11812405+glacier74@users.noreply.github.com>
2022-03-11 10:10:50 +0800

    feat(dashboard): add sentry error fallback component

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -2,6 +2,8 @@ import Router from '@/router'
 import { store, StoreProvider } from '@/store'
 import { getDeviceId, setDeviceId } from '@/utils'
 import { register } from '@/utils/serviceWorker'
+import { EmojiSadIcon } from '@heroicons/react/outline'
+import { EmptyStates } from '@heyforms/ui'
 import * as Sentry from '@sentry/react'
 import { Suspense } from 'react'
 import 'react-app-polyfill/ie11'
@@ -16,8 +18,17 @@ if (!getDeviceId()) {
 }
 
 const App = () => {
+  const Fallback = (
+    <EmptyStates
+      className="h-screen flex flex-col justify-center"
+      icon={<EmojiSadIcon />}
+      title="Oops, Something went wrong"
+      description="Brace yourself till we get the error fixed. You may also refresh the page or try again later."
+    />
+  )
+
   return (
-    <Sentry.ErrorBoundary fallback={<></>}>
+    <Sentry.ErrorBoundary fallback={Fallback}>
       <Suspense fallback={<></>}>
         <StoreProvider value={store}>
           <Router />
