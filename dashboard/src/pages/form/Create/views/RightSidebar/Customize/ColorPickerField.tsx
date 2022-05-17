3af0d504a2d7aa7e7c92416bc4ac65ab2021aa5a
Glacier <11812405+glacier74@users.noreply.github.com>
2022-05-17 16:18:36 +0800

    Refactor ColorPicker

--- dashboard/src/pages/form/Create/views/RightSidebar/Customize/ColorPickerField.tsx
+++ dashboard/src/pages/form/Create/views/RightSidebar/Customize/ColorPickerField.tsx
@@ -1,24 +1,7 @@
+import { COLOR_PICKER_PRESET_COLORS } from '@/consts'
 import { ChevronDownIcon } from '@heroicons/react/outline'
-import { Button, Dropdown, Menus, stopPropagation } from '@heyforms/ui'
+import { Button, ColorPicker, Dropdown, Menus, stopPropagation } from '@heyforms/ui'
 import type { FC } from 'react'
-import { startTransition } from 'react'
-import type { ColorResult } from 'react-color'
-import { EditableInput, Hue, Saturation } from 'react-color/lib/components/common'
-import ColorWrap from 'react-color/lib/components/common/ColorWrap'
-
-interface WrapperProps {
-  color?: string
-  presets?: string[]
-  hsl?: any
-  hsv?: any
-  hex?: string
-  onChange?: (color: ColorResult) => void
-}
-
-interface ColorPickerProps {
-  color?: string
-  onChange?: (color: string) => void
-}
 
 interface ColorPickerFieldProps {
   label: string
@@ -26,67 +9,24 @@ interface ColorPickerFieldProps {
   onChange?: (value: string) => void
 }
 
-const SaturationPointer = () => <div className="color-picker-saturation-pointer" />
-const HuePointer = () => <div className="color-picker-hue-pointer" />
-
-const Wrapper: FC<WrapperProps> = ({ hsl, hsv, hex, presets = [], onChange }) => {
-  return (
-    <div className="color-picker-popup" onClick={stopPropagation}>
-      <div className="color-picker-saturation">
-        <Saturation
-          hsl={hsl}
-          hsv={hsv}
-          pointer={SaturationPointer as any}
-          onChange={onChange as any}
-        />
-      </div>
-      <div className="color-picker-hue">
-        <Hue hsl={hsl} hsv={hsv} pointer={HuePointer as any} onChange={onChange as any} />
-      </div>
-      <div className="color-picker-editable">
-        <div
-          className="color-picker-preview"
-          style={{
-            background: hex
-          }}
-        />
-        <div className="color-picker-editable-input">
-          <EditableInput value={hex} onChange={onChange} />
-        </div>
-      </div>
-    </div>
-  )
-}
-const ColorWrapper = ColorWrap(Wrapper as any)
-
-const ColorPicker: FC<ColorPickerProps> = ({ color, onChange }) => {
-  function handleChange({ hex }: ColorResult) {
-    startTransition(() => {
-      onChange?.(hex)
-    })
-  }
-
+export const ColorPickerField: FC<ColorPickerFieldProps> = ({ label, value, onChange }) => {
   const Overlay = (
     <Menus>
-      <ColorWrapper color={color} onChange={handleChange} />
+      <div className="color-picker-popup" onClick={stopPropagation}>
+        <ColorPicker color={value} presetColors={COLOR_PICKER_PRESET_COLORS} onChange={onChange} />
+      </div>
     </Menus>
   )
 
   return (
-    <Dropdown overlay={Overlay}>
-      <Button className="color-picker" trailing={<ChevronDownIcon />}>
-        <div className="color-picker-value" style={{ backgroundColor: color }}></div>
-      </Button>
-    </Dropdown>
-  )
-}
-
-export const ColorPickerField: FC<ColorPickerFieldProps> = ({ label, value, onChange }) => {
-  return (
     <div className="right-sidebar-settings-item">
       <div className="flex items-center justify-between">
         <label className="form-item-label">{label}</label>
-        <ColorPicker color={value} onChange={onChange} />
+        <Dropdown overlay={Overlay}>
+          <Button className="color-picker" trailing={<ChevronDownIcon />}>
+            <div className="color-picker-value" style={{ backgroundColor: value }}></div>
+          </Button>
+        </Dropdown>
       </div>
     </div>
   )
