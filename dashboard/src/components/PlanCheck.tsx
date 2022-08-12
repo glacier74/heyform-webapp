96273774cd8fd2871c11af2b73e9b0f62ecb5e6d
Glacier <11812405+glacier74@users.noreply.github.com>
2022-08-12 12:48:39 +0800

    Add badge to PlanCheck

--- dashboard/src/components/PlanCheck.tsx
+++ dashboard/src/components/PlanCheck.tsx
@@ -1,5 +1,6 @@
 import { PlanGradeEnum } from '@/models'
 import { useStore } from '@/store'
+import { Badge } from '@heyforms/ui'
 import { observer } from 'mobx-react-lite'
 import { FC } from 'react'
 
@@ -21,7 +22,9 @@ export const PlanCheck: FC<UpgradeButtonProps> = observer(({ permission, childre
     <div className="plan-check relative">
       {children}
       {grade < permission && (
-        <div className="absolute inset-0 z-10 cursor-pointer" onClick={handleClick} />
+        <div className="flex items-center justify-end absolute inset-0 px-2 py-1 z-10 cursor-pointer" onClick={handleClick}>
+          <Badge type="blue" text="Pro" rounded />
+        </div>
       )}
     </div>
   )
