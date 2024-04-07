8c3c94da7b5a4d324f10563767cb181366aadb6c
Glacier <11812405+glacier74@users.noreply.github.com>
2024-04-07 15:49:59 +0800

    Fix OpinionScaleCell error

--- dashboard/src/pages/form/Submissions/views/Sheet/sheetCells/OpinionScaleCell.tsx
+++ dashboard/src/pages/form/Submissions/views/Sheet/sheetCells/OpinionScaleCell.tsx
@@ -1,13 +1,16 @@
-import { FC } from 'react'
+import { isObject, isValid } from '@hpnp/utils/helper'
+import { FC, useMemo } from 'react'
 
 import { SheetCellProps } from '../types'
 
 export const OpinionScaleCell: FC<SheetCellProps> = ({ column, row }) => {
-  const value = row[column.key]
+  const value = useMemo(() => {
+    const v = row[column.key]
+
+    return [isObject(v) ? undefined : v, column.properties?.total].filter(isValid).join(' / ')
+  }, [column.key, column.properties?.total, row])
 
   return (
-    <div className="heygrid-cell-text overflow-hidden text-ellipsis whitespace-nowrap">
-      {value} / {column.properties?.total}
-    </div>
+    <div className="heygrid-cell-text overflow-hidden text-ellipsis whitespace-nowrap">{value}</div>
   )
 }
